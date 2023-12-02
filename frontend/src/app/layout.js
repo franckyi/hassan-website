622c7d0bc1fd5b102ebcee6934f5a9193e67bd0b
Francky <contact@francky.pl>
2023-12-02 14:25:42 +0100

    update meta

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Lora } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const FONT = Lora({ subsets: ["latin"] });
 
@@ -11,6 +12,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pl">
+      <Head>
+        <meta name="robots" content="noindex,nofollow" />
+        <title>Hassan - Pizzeria & Kebab</title>
+        <meta
+          name="description"
+          content="Nasza restauracja to miejsce, gdzie tradycja orientalnej kuchni łączy się z niepowtarzalnymi smakami, przywołującymi podróże kulinarne do Bliskiego Wschodu. Zapraszamy do skosztowania naszych wyjątkowych kebabów, które stanowią idealne połączenie tradycji i świeżości."
+        ></meta>
+      </Head>
       <body className={FONT.className}>{children}</body>
     </html>
   );
