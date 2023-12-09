7336f570bae32f4085d7aefe34c0a274b23550ff
Francky <contact@francky.pl>
2023-12-09 20:03:13 +0100

    use https

--- frontend/src/app/services/getBusinessInfo.jsx
+++ frontend/src/app/services/getBusinessInfo.jsx
@@ -16,7 +16,7 @@ const businessInfoFallback = {
 
 async function getBusinessInfo() {
   const res = await fetch(
-    "http://panel.kebab-hassan.pl/wp-json/wp/v2/stores/42"
+    "https://panel.kebab-hassan.pl/wp-json/wp/v2/stores/42"
   );
 
   if (!res.ok) {
