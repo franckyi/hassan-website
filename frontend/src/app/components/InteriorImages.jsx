e2f7ba9629b4e372abf5415c8d8f85ec23d0168d
Francky <contact@francky.pl>
2023-12-16 13:10:41 +0100

    use CMS gallery with fallback on local images

--- frontend/src/app/components/InteriorImages.jsx
+++ frontend/src/app/components/InteriorImages.jsx
@@ -1,16 +1,36 @@
 import { useState, useEffect } from "react";
-import Image from "next/image";
 import getBusinessInfo from "../services/getBusinessInfo";
 
-const images = [
-  { src: "/local-1.webp" },
-  { src: "/local-2.webp" },
-  { src: "/local-3.webp" },
-  { src: "/food-1.webp" },
+const homeGalleryImagesUrl = "https://panel.kebab-hassan.pl/wp-json/wp/v2/media?parent=109";
+
+const fallbackImages = [
+  { source_url: "/local-1.webp", alt_text: "wnętrz restauracji" },
+  { source_url: "/local-2.webp", alt_text: "wnętrz restauracji" },
+  { source_url: "/local-3.webp", alt_text: "wnętrz restauracji" },
+  { source_url: "/food-1.webp", alt_text: "zdjęcie potraw" },
 ];
 
 export default function InteriorImages() {
   const [businessInfo, setBusinessInfo] = useState(null);
+  const [images, setImages] = useState(null);
+
+  async function getImages() {
+    try {
+      const res = await fetch(homeGalleryImagesUrl);
+  
+      if (!res.ok) {
+        setImages(fallbackImages);
+        return;
+      }
+  
+      const imagesResponse = await res.json();
+      console.log("imagesResponse", imagesResponse);
+      setImages(imagesResponse);
+    } catch (error) {
+      console.error("Nie mogłem pobierać aktualne zdjęcia", error);
+      setImages(fallbackImages);
+    }
+  }
 
   const fetchBusinessInfo = async () => {
     try {
@@ -25,25 +45,27 @@ export default function InteriorImages() {
   };
 
   useEffect(() => {
+    getImages();
     fetchBusinessInfo();
   }, []);
+
   return (
     <>
-      <article className="w-full lg:flex">
-        {images.map((img, index) => {
+      {images && <article className="w-full lg:flex">
+        {images && images.map((img) => {
           return (
-            <Image
-              key={index}
-              src={img.src}
+            <img
+              key={img.id}
+              src={img.source_url}
               className="w-full lg:w-1/4 lg:hover:scale-110 lg:ease-in lg:duration-150"
               width={400}
               height={400}
-              alt="wnętrz restauracji"
+              alt={img.alt_text}
               draggable="false"
             />
-          );
+            );
         })}
-      </article>
+      </article>}
 
       {businessInfo && (
         <p className="text-xl lg:w-2/4 mx-8 my-16 text-center">
