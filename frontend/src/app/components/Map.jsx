e2c61c1a803bfd8e89895421486a6c929434d550
Francky <contact@francky.pl>
2024-04-05 12:42:05 +0200

    clean

--- frontend/src/app/components/Map.jsx
+++ frontend/src/app/components/Map.jsx
@@ -23,15 +23,5 @@ export default function Map(props) {
         </Popup>
       </Marker>
     </MapContainer>
-    // <MapContainer
-    //   center={[40.8054, -74.0241]}
-    //   zoom={14}
-    //   scrollWheelZoom={false}
-    //   style={{ height: "100%", width: "100%" }}
-    // >
-    //   <Marker position={[40.8054, -74.0241]} draggable={true} animate={true}>
-    //     <Popup>Hey ! you found me</Popup>
-    //   </Marker>
-    // </MapContainer>
   );
 }
